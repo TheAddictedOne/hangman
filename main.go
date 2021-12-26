39ac6bfc57e02276ecaff25f3cd841c808dc48e2
David Auk <auk.david@gmail.com>
2021-12-26 13:29:10 +0100

    Plug static templates

--- main.go
+++ main.go
@@ -2,17 +2,34 @@ package main
 
 import (
 	"net/http"
+	"text/template"
 )
 
-type testHandler struct {
-	Message string
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	homepage := template.Must(template.ParseFiles("views/index.html"))
+
+	switch r.Method {
+	case http.MethodGet:
+		homepage.Execute(w, nil)
+	default:
+		w.Write([]byte("Method not handled"))
+	}
 }
 
-func (f *testHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(f.Message))
+func gameHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		// Show game
+	case http.MethodPost:
+		// Get letter
+	}
 }
 
 func main() {
-	http.Handle("/", &testHandler{Message: "homepage"})
+	fileserver := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static", fileserver))
+
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/game", gameHandler)
 	http.ListenAndServe(":8080", nil)
 }
