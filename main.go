08325dd2a1262b45b3f150ee7e7f9e49bfe52910
David Auk <auk.david@gmail.com>
2021-12-26 17:26:11 +0100

    Lot of working things

--- main.go
+++ main.go
@@ -1,35 +1,113 @@
 package main
 
 import (
+	"bufio"
+	"math/rand"
 	"net/http"
+	"os"
 	"text/template"
+	"time"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	homepage := template.Must(template.ParseFiles("views/index.html"))
+var state State
 
+type Letter struct {
+	Value string
+	Used  bool
+}
+
+type State struct {
+	Level        string
+	CompleteWord string
+	Letters      []Letter
+	Errors       int
+	CurrentWord  string
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	page := template.Must(template.ParseFiles("views/index.html"))
+
+	page.Execute(w, state)
+}
+
+func selectLevelHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
+
 	case http.MethodGet:
-		homepage.Execute(w, nil)
+		http.ServeFile(w, r, "views/select-level.html")
+
+	case http.MethodPost:
+		r.ParseForm()
+		state.Level = r.FormValue("level")
+		http.Redirect(w, r, "/", http.StatusMovedPermanently)
+
 	default:
-		w.Write([]byte("Method not handled"))
+		http.Redirect(w, r, "/", http.StatusMovedPermanently)
 	}
 }
 
 func gameHandler(w http.ResponseWriter, r *http.Request) {
+	page := template.Must(template.ParseFiles("views/game.html"))
+
 	switch r.Method {
 	case http.MethodGet:
-		// Show game
+		word := getNewWord(state.Level)
+		state.CompleteWord = word
+		state.Letters = initializeLetters()
+		state.Errors = 0
+		state.CurrentWord = initializeCurrentWord(len(word))
+		page.Execute(w, state)
+
 	case http.MethodPost:
-		// Get letter
+		page.Execute(w, state)
 	}
 }
 
+func getNewWord(level string) string {
+	file, _ := os.Open("files/" + level + ".txt")
+	scanner := bufio.NewScanner(file)
+	var words []string
+
+	for scanner.Scan() {
+		words = append(words, scanner.Text())
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	num := rand.Intn(len(words))
+
+	return words[num]
+}
+
+func initializeLetters() []Letter {
+	var letters []Letter
+	a := 97
+	total := 26
+
+	for i := a; i < a+total; i++ {
+		letters = append(letters, Letter{Value: string(i), Used: false})
+	}
+
+	return letters
+}
+
+func initializeCurrentWord(n int) string {
+	var s string
+
+	for i := 0; i < n; i++ {
+		s += "_"
+	}
+
+	return s
+}
+
 func main() {
+	state.Level = "jrpgs"
+
 	fileserver := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static", fileserver))
 
 	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/game", gameHandler)
+	http.HandleFunc("/select-level", selectLevelHandler)
+	http.HandleFunc("/play", gameHandler)
 	http.ListenAndServe(":8080", nil)
 }
